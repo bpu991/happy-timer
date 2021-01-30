8ce4fb10cc3836d02e0db9b0d3b18beed49f94ab
bpu991 <bpu991@gmail.com>
2021-01-30 18:45:08 -0500

    added more timer functionality

--- src/Timer.js
+++ src/Timer.js
@@ -6,13 +6,15 @@ class Timer extends Component {
         super();
         this.state = {
             minutes: 0,
+            minutes2: 0,
             seconds: 0,
+            seconds2: 0,
             running: false
         }
     }
 
     incrementSeconds = () => {
-        if (this.state.seconds < 60) {
+        if (this.state.seconds < 5) {
             this.setState({seconds: this.state.seconds + 1})
         }
     }
@@ -24,8 +26,21 @@ class Timer extends Component {
 
     }
 
+    incrementSeconds2 = () => {
+        if (this.state.seconds2 < 9) {
+            this.setState({seconds2: this.state.seconds2 + 1})
+        }
+    }
+
+    decrementSeconds2 = () => {
+        if (this.state.seconds2 > 0) {
+            this.setState({seconds2: this.state.seconds2 - 1})
+        }
+
+    }
+
     incrementMinutes = () => {
-        if (this.state.minutes < 60) {
+        if (this.state.minutes < 5) {
             this.setState({minutes: this.state.minutes + 1})
         }
     }
@@ -35,18 +50,47 @@ class Timer extends Component {
             this.setState({minutes: this.state.minutes - 1})
         }
     }
+    incrementMinutes2 = () => {
+        if (this.state.minutes2 < 9) {
+            this.setState({minutes2: this.state.minutes2 + 1})
+        }
+    }
+
+    decrementMinutes2 = () => {
+        if (this.state.minutes2 > 0) {
+            this.setState({minutes2: this.state.minutes2 - 1})
+        }
+    }
 
     startTimer = () => {
+        // let interval = setInterval(() => {
+        //     if (this.state.minutes >= 0 && this.state.seconds > 0) {
+        //         this.setState({running: true, seconds: this.state.seconds - 1})
+        //     } else if (this.state.minutes > 0 && this.state.seconds === 0) {
+        //         this.setState({running: true, minutes: this.state.minutes - 1, seconds: 59})
+        //     } else if (this.state.minutes === 0 && this.state.seconds > 0) {
+        //         this.setState({running: true, minutes: 0, seconds: 50})
+        //     } else {
+        //         clearInterval(interval)
+        //         this.setState({running:false})
+        //     }
+        // }, 1000)
         let interval = setInterval(() => {
-            if (this.state.minutes >= 0 && this.state.seconds > 0) {
-                this.setState({running: true, seconds: this.state.seconds - 1})
-            } else if (this.state.minutes > 0 && this.state.seconds === 0) {
-                this.setState({running: true, minutes: this.state.minutes - 1, seconds: 59})
-            } else if (this.state.minutes === 0 && this.state.seconds > 0) {
-                this.setState({running: true, minutes: 0, seconds: 50})
-            } else {
-                clearInterval(interval)
-                this.setState({running:false})
+            if(this.state.minutes >= 0 && this.state.minutes2 >= 0 && this.state.seconds > 0 && this.state.seconds2 > 0) {
+                console.log('1')
+                this.setState({running: true, seconds2: this.state.seconds2 - 1})
+            } else if (this.state.minutes > 0 && this.state.minutes2 > 0 && this.state.seconds > 0 && this.state.seconds2 === 0) {
+                console.log('2')
+                this.setState({running: true, seconds: this.state.seconds - 1, seconds2: 9})
+            } else if (this.state.minutes > 0 && this.state.minutes2 > 0 && this.state.seconds === 0 && this.state.seconds2 > 0) {
+                console.log('3')
+                this.setState({running: true, seconds2: this.state.seconds2 - 1})
+            } else if (this.state.minutes > 0 && this.state.minutes2 === 0 && this.state.seconds === 0 && this.state.seconds2 === 0) {
+                console.log('4')
+                this.setState({running: true, minutes2: this.state.minutes2 - 1, seconds: 5, seconds2: 9})
+            } else if(this.state.minutes > 1 && this.state.minutes2 === 0, this.state.seconds === 0, this.state.seconds2 === 0) {
+                console.log('5')
+                this.setState({running: true, minutes: 0, minutes2: 9, seconds: 5, seconds2: 9})
             }
         }, 1000)
     }
@@ -54,12 +98,14 @@ class Timer extends Component {
         return (
             <div className='timer-body'>
                 {this.state.running === true ?
-                    <h1>{this.state.minutes}:{this.state.seconds}</h1>
+                    <h1>{this.state.minutes}{this.state.minutes2}:{this.state.seconds}{this.state.seconds2}</h1>
                     :
                     <div>
                         <button className='minInc'onClick={this.incrementMinutes}>^</button>
+                        <button className='minInc'onClick={this.incrementMinutes2}>^</button>
                         <button className='secInc'onClick={this.incrementSeconds}>^</button>
-                        <h1>{this.state.minutes}:{this.state.seconds}</h1>
+                        <button className='secInc'onClick={this.incrementSeconds2}>^</button>
+                        <h1>{this.state.minutes}{this.state.minutes2}:{this.state.seconds}{this.state.seconds2}</h1>
                         <button className='minDec'onClick={this.decrementMinutes}>v</button>
                         <button className='secDec'onClick={this.decrementSeconds}>v</button>
                         <button onClick={this.startTimer}>Start Timer</button>
