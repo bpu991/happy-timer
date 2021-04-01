446f25b4302d2aa3d3724d2af3376cda93afbe0b
Ben Pu <bpu991@gmail.com>
2021-04-01 13:04:37 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Timer from './components/Timer';
-import AlternateTimer from './components/AlternateTimer'
 
 function App() {
   return (
     <div className="App">
-      {/* <AlternateTimer /> */}
       <Timer />
     </div>
   );
